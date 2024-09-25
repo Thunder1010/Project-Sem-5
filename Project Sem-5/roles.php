ac4dd6f876776c6b46fb76ab468aec3cb951a045
Thunder1010 <166369394+Thunder1010@users.noreply.github.com>
2024-09-25 10:09:42 +0530

    Delete Project Sem-5 directory

--- Project Sem-5/roles.php
+++
@@ -1,9 +0,0 @@
-<?
-if ($role == 'hod') {
-    header('Location: hod_dashboard.html');
-} elseif ($role == 'teacher') {
-    header('Location: teacher_dashboard.html');
-} elseif ($role == 'student') {
-    header('Location: student_dashboard.html');
-}
->
